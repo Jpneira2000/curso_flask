c52ba7cecefcba0f42620fb16c27c101273aa506
jpneira2000 <jpneira2000@gmail.com>
2023-11-29 13:54:04 -0600

    Clase configurar páginas de error

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ app = Flask(__name__)
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video a productor']
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internal_server(error):
+    return render_template('500.html', error=error)
+
 @app.route('/')
 def index():
-    user_ip = request.remote_addr
+    raise(Exception('500 error'))
+    # user_ip = request.remote_addr
 
-    response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    # response = make_response(redirect('/hello'))
+    # response.set_cookie('user_ip', user_ip)
 
-    return response
+    # return response
 
 @app.route('/hello')
 def hello():
